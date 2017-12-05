a9a8c1bb19e2981fcac3129871a69ed209ef08eb
129472 <129472@INDSNOIDV3K622.AMERICAS.GLOBAL.NTTDATA.COM>
2017-12-05 14:02:57 +0530

    Spring Lifecycle demo

---
+++ lifecycle_demo/src/main/java/lifecycle/springframework/DiDemoApplication.java
@@ -0,0 +1,15 @@
+package lifecycle.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Vivek on 12/5/17.
+ */
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+}
